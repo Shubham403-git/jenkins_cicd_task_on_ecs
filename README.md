8c5578632293211d86bb3ee8aad2ef33802ed40d
Shubham kumar <you@example.com>
2021-11-08 22:23:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Jenkins Ci/Cd task to vbuild Docker image on every new code commit, and host the new docker image using AWS.
+Jenkins/Docker/AWS ECR & ECS. 
+
+
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import *
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload():
+    return "task successfull"
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
