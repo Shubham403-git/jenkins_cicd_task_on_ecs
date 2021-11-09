1a0320dc1203d9bf16ecb2ba2bbd847797f2b34e
Shubham403-git <70068526+Shubham403-git@users.noreply.github.com>
2021-11-09 18:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def upload():
+def docker():
     return " Runing Inside docker Conatiner(flask App) "
-@app.route('/task')
-def taskdone():
-    return "Task Done Successfully With CI-CD implementation."
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
