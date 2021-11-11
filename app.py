585339309797ab73adae3b383252e26b0399ecd0
Shubham403-git <70068526+Shubham403-git@users.noreply.github.com>
2021-11-11 19:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def docker():
-    return " Runing Inside docker Conatiner(flask App) "
+    return " Hello World "
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
